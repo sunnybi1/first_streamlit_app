cfea3db707ec04abea65d3e4465a8e9bc7e554f1
sunnybi1 <78920522+sunnybi1@users.noreply.github.com>
2023-05-10 11:06:12 -0600

    Update streamlit_app.py
    added some text to the header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.comment(' Omega 3 & Blueberry Oatmeal')
+streamlit.comment(' Kale, Spinach & Rocket Smoothie')
+streamlit.comment(' Hard-Boiled Free-Range Egg')
+                  
